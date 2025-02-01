203b28551609495c9c9c380d2c76d80627a6ad6a
ryancwalsh <rcwalsh@gmail.com>
2025-02-01 17:15:12 -0500

    2024-04-18 15:38:59 -0400 88a1d1b  (HEAD -> main) #youtube-extension-text-box-centerer styles

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,11 +2,15 @@
 /* global document */
 /* global window */
 (function () {
-  let savedPlaybackRate = 1; // Default playback speed
+  const savedPlaybackRate = 1; // Default playback speed
 
   function inputActive(currentElement) {
     // If on an input or textarea
-    if (currentElement.tagName.toLowerCase() === 'input' || currentElement.tagName.toLowerCase() === 'textarea' || currentElement.isContentEditable) {
+    if (
+      currentElement.tagName.toLowerCase() === "input" ||
+      currentElement.tagName.toLowerCase() === "textarea" ||
+      currentElement.isContentEditable
+    ) {
       return true;
     } else {
       return false;
@@ -19,49 +23,53 @@
     const timer = setInterval(function () {
       if (opacity <= 0.1) {
         clearInterval(timer);
-        element.style.display = 'none';
+        element.style.display = "none";
       }
 
       element.style.opacity = opacity;
-      element.style.filter = 'alpha(opacity=' + opacity * 100 + ')';
+      element.style.filter = "alpha(opacity=" + opacity * 100 + ")";
       opacity -= opacity * 0.1;
     }, 50);
   }
 
   function displayTextOverlay(content, boundingElement) {
-    const elementId = 'youtube-extension-text-box';
-    const HTML = '<div id="' + elementId + '">' + content + '</div>';
+    const containerElementId = "youtube-extension-text-box-centerer";
+    const elementId = "youtube-extension-text-box";
+    const containerHTML = `<div id="${containerElementId}"><div id="${elementId}">${content}</div></div>`;
+    const containerElement = document.querySelector(
+      "#youtube-extension-text-box"
+    );
     let element = document.getElementById(elementId);
 
     // If the element doesn't exist, append it to the body.
-    if (element) {
+    if (containerElement) {
       element.innerHTML = content;
     } else {
-      boundingElement.insertAdjacentHTML('afterbegin', HTML);
+      boundingElement.insertAdjacentHTML("afterbegin", containerHTML);
       element = document.getElementById(elementId);
     }
 
-    element.style.display = 'block';
+    element.style.display = "block";
     const opacity = 0.8;
     element.style.opacity = opacity;
-    element.style.filter = 'alpha(opacity=' + opacity * 100 + ')';
+    element.style.filter = "alpha(opacity=" + opacity * 100 + ")";
     setTimeout(function () {
       fadeout(element, opacity);
     }, 2_500);
   }
 
   function displayLabelInLogo(speed) {
-    const logoLabelId = 'youtube-extension-label-in-logo';
-    const HTML = '<div id="' + logoLabelId + '">' + speed + 'x</div>';
-    let element = document.getElementById(logoLabelId);
+    const logoLabelId = "youtube-extension-label-in-logo";
+    const HTML = '<div id="' + logoLabelId + '">' + speed + "x</div>";
+    const element = document.getElementById(logoLabelId);
 
     // If the element doesn't exist, append it to the body.
     if (element) {
-      element.innerHTML = speed + 'x';
+      element.innerHTML = speed + "x";
     } else {
-      const boundingElement = document.querySelector('#logo');
+      const boundingElement = document.querySelector("#logo");
       if (boundingElement) {
-        boundingElement.insertAdjacentHTML('afterbegin', HTML);
+        boundingElement.insertAdjacentHTML("afterbegin", HTML);
       }
     }
   }
@@ -80,7 +88,10 @@
     if (video) {
       video.playbackRate = rate;
       console.log(`setPlaybackRate ${video.playbackRate}x`);
-      displayTextOverlay(`${video.playbackRate}x`, document.querySelector('#movie_player'));
+      displayTextOverlay(
+        `${video.playbackRate}x`,
+        document.querySelector("#movie_player")
+      );
       displayLabelInLogo(video.playbackRate);
     }
   }
@@ -88,9 +99,9 @@
   window.onkeyup = function (event) {
     const key = event.key;
     // ctrlKey = event.ctrlKey,
-    const video = document.querySelector('video');
-    const mediaElement = document.querySelector('#movie_player');
-    const mediaElementChildren = mediaElement.querySelectorAll('*');
+    const video = document.querySelector("video");
+    const mediaElement = document.querySelector("#movie_player");
+    const mediaElementChildren = mediaElement.querySelectorAll("*");
     const activeElement = document.activeElement;
     let index;
 
@@ -99,10 +110,10 @@
       return;
     }
 
-    if (['[', ']'].includes(key)) {
-      if (key === '[') {
+    if (["[", "]"].includes(key)) {
+      if (key === "[") {
         setPlaybackRate(video, increment(video.playbackRate, -0.1));
-      } else if (key === ']') {
+      } else if (key === "]") {
         setPlaybackRate(video, increment(video.playbackRate, 0.1));
       }
     }
@@ -110,7 +121,10 @@
     // Check if the media element, or any of its children are active.
     // Else we'll be overwriting the previous actions.
     for (index = 0; index < mediaElementChildren.length; index += 1) {
-      if (mediaElementChildren[index] === activeElement || mediaElement === activeElement) {
+      if (
+        mediaElementChildren[index] === activeElement ||
+        mediaElement === activeElement
+      ) {
         return;
       }
     }
@@ -122,6 +136,11 @@
     //   video.currentTime -= jumpBackSeconds;
     //   displayTextOverlay(`Back ${jumpBackSeconds} sec`, mediaElement);
     // }
+    if (key === "b") {
+      const jumpBackSeconds = 10;
+      video.currentTime -= jumpBackSeconds;
+      displayTextOverlay(`↩️ Back ${jumpBackSeconds} sec`, mediaElement);
+    }
   };
 
   // // Save playback rate before ad plays and restore it after. (This came from ChatGPT but might not work and might hang the browser.)
